b71682b5ce839c8d25c48397a0b99d751ddd4d6d
miltonyela <miltonyela@gmail.com>
2023-07-18 20:12:02 -0500

    se crean objetos de usuarios

---
+++ main.js
@@ -0,0 +1,23 @@
+usuarios={
+            id:1,
+            nombre:"Santiago",
+            sexo:"Masculino",
+            celular:3002947546,
+
+}
+
+mandadero={
+    id:1,
+    nombre:"Cristian",
+    sexo:"Masculino",
+    celular:3202989546,
+    
+}
+
+admin={
+    id:1,
+    nombre:"Ximena",
+    sexo:"Femenino",
+    celular:3182457148,
+    
+}
